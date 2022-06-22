4fb5165cd14eb53802fbbeade5a22db93301e749
AleGC019 <00083521@uca.edu.sv>
2022-06-22 00:46:01 -0600

    :zap:EventosCasiCompleto

--- BINAES_Proyecto/BINAES_Proyecto/Evento.cs
+++ BINAES_Proyecto/BINAES_Proyecto/Evento.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,9 +13,12 @@ namespace BINAES_Proyecto
     {
         public int ID { get; set; }
         
+        public int id_area { get; set; }
         public string Titulo_Evento { get; set; }
         
         public string Portada { get; set; }
+        
+        public Bitmap Imagen_del_evento { get; set; }
         
         public int Cantidad_de_asistentes { get; set; }
         
@@ -25,6 +29,7 @@ namespace BINAES_Proyecto
         public string Area { get; set; }
         
         public string Objetivos { get; set; }
+        
         
         
 
--- BINAES_Proyecto/BINAES_Proyecto/Forms/frmEvento.cs
+++ BINAES_Proyecto/BINAES_Proyecto/Forms/frmEvento.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Windows.Forms;
 
 namespace BINAES_Proyecto.Forms
@@ -11,11 +12,13 @@ namespace BINAES_Proyecto.Forms
             InitializeComponent();
         }
 
+        
+        
         private void btnBuscar_Click(object sender, EventArgs e)
         {
             if(cmbEvento.Text == "Nombre del evento") // Aqui pones el nombre que le hayas puesto a la combobox
              { 
-                List<Evento> resultado = BuscarEventoDAO.NombreEvento(txtBuscarEvento.Text.Trim());
+                List<Evento> resultado = EventoDAO.NombreEvento(txtBuscarEvento.Text.Trim());
                 txtBuscarEvento.Clear();
 
                 dgvEvento.DataSource = null;
@@ -27,8 +30,12 @@ namespace BINAES_Proyecto.Forms
                     DataGridViewContentAlignment.MiddleCenter;
                 dgvEvento.Columns["Titulo_Evento"].DefaultCellStyle.Alignment =
                     DataGridViewContentAlignment.MiddleCenter;
-                dgvEvento.Columns["Portada"].DefaultCellStyle.Alignment =
-                    DataGridViewContentAlignment.MiddleCenter;
+                
+                
+                dgvEvento.Columns["Portada"].Visible = false;
+                dgvEvento.Columns["id_area"].Visible = false;
+                
+                
                 dgvEvento.Columns["Cantidad_de_asistentes"].DefaultCellStyle.Alignment =
                     DataGridViewContentAlignment.MiddleCenter;
                 dgvEvento.Columns["Inicio_del_vento"].DefaultCellStyle.Alignment =
@@ -44,7 +51,7 @@ namespace BINAES_Proyecto.Forms
 
             else
             {
-                List<Evento> resultado = BuscarEventoDAO.Eventos();
+                List<Evento> resultado = EventoDAO.Eventos();
                 txtBuscarEvento.Clear();
 
                 dgvEvento.DataSource = null;
@@ -54,9 +61,13 @@ namespace BINAES_Proyecto.Forms
                     DataGridViewContentAlignment.MiddleCenter;
                 dgvEvento.Columns["Titulo_Evento"].DefaultCellStyle.Alignment =
                     DataGridViewContentAlignment.MiddleCenter;
-                dgvEvento.Columns["Portada"].DefaultCellStyle.Alignment =
-                    DataGridViewContentAlignment.MiddleCenter;
-                dgvEvento.Columns["Cantidad_de_asistentes"].DefaultCellStyle.Alignment =
+
+
+                dgvEvento.Columns["Portada"].Visible = false;
+                dgvEvento.Columns["id_area"].Visible = false;
+
+
+                    dgvEvento.Columns["Cantidad_de_asistentes"].DefaultCellStyle.Alignment =
                     DataGridViewContentAlignment.MiddleCenter;
                 dgvEvento.Columns["Inicio_del_vento"].DefaultCellStyle.Alignment =
                     DataGridViewContentAlignment.MiddleCenter;
@@ -66,7 +77,65 @@ namespace BINAES_Proyecto.Forms
                     DataGridViewContentAlignment.MiddleCenter;
                 dgvEvento.Columns["Objetivos"].DefaultCellStyle.Alignment =
                     DataGridViewContentAlignment.MiddleCenter;
+                
+                
+                
             }
+        }
+        
+        
+
+        public string imagen { get; set; }
+        
+        private void btnAgregar_click(object sender, EventArgs e)
+        {
+            OpenFileDialog obtener = new OpenFileDialog();
+            obtener.InitialDirectory = "C:\\";
+            obtener.Filter = "Image Files(*.jpg; *.jpeg; *.gif; *.bmp, *.png)| *.jpg; *.jpeg; *.gif; *.bmp, *.png";
+            /*
+            if(obtener.ShowDialog() = DialogResult.OK)
+            {
+                imagen = obtener.FileName;
+            }
+            else
+            {
+                MessageBox.Show("No se ha seleccionado una foto");
+            }
+            */
+        }
+
+
+        private void frmEvento_Load(object sender, EventArgs e)
+        {
+            cmbEArea.DataSource = null;
+            cmbEArea.ValueMember = "areaID";
+            cmbEArea.DisplayMember = "areaNombre";
+            cmbEArea.DataSource = EventoDAO.CargarDatosArea();
+
+
+
+
+            dtpIniico.Format = DateTimePickerFormat.Time;
+
+            dtpFinalizacion.Format = DateTimePickerFormat.Time;
+
+        }
+
+        private void btnAgregarE_Click(object sender, EventArgs e)
+        {
+            
+            Evento eve = new Evento();
+
+            eve.Titulo_Evento = txtNombreE.Text;
+            eve.Cantidad_de_asistentes = Convert.ToInt32(nudCantidad.Value);
+            eve.id_area = Convert.ToInt32(cmbEArea.SelectedValue);
+            eve.Inicio_del_vento = Convert.ToDateTime(dtpIniico.Value);
+            eve.Finalizacion_del_evento = Convert.ToDateTime(dtpFinalizacion.Value);
+            eve.Portada = "0000";
+            eve.Objetivos = txtobjetivo.Text;
+            
+            EventoDAO.IngresarEvento(eve);
+            MessageBox.Show("Ingresado con exito");
         }
     }
 }
